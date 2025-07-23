c391ef977cc2e799c60f545c8f598e78161ae0a3
Bùi Thiện <21110656@student.hcmute.edu.vn>
2025-07-23 11:14:16 +0700

    first commit

---
+++ account/model/interface.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	Get(ctx context.Context, uid uuid.UUID) (*User, error)
+}
+type UserRepository interface {
+	FindByID(ctx context.Context, uid uuid.UUID) (*User, error)
+}
